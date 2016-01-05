c44b9d8300a76a83bf41caea5a63a480916606d5
unknown <sheravanan@gmail.com>
2016-01-05 18:00:10 +1100

    gulp

--- todolist/gulpfile.js
+++ todolist/gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp");
 var connect = require("gulp-connect");
 var uglify = require("gulp-uglify");
 
+//create gulp server
+
 gulp.task("server", function(){
 
 	connect.server({
@@ -9,13 +11,14 @@ gulp.task("server", function(){
 		livereload :true
 	});
 });
+// live reload task created
 
 gulp.task("livereload",function (){
 
 	return gulp.src(["directive/**", "!bower_components","!node_modules"])
 	.pipe(connect.reload());
 })
-
+// js minified
 gulp.task("minified", function (){
 
 gulp.src("js/**/*.js").
@@ -23,11 +26,9 @@ gulp.src("js/**/*.js").
 	pipe(gulp.dest("build/"));
 
 });
-
+// watches the live-reload 
 gulp.task("watcher", function (){
-
 return gulp.watch(["./**/*"],["livereload"]);
-
 });
 
 gulp.task("default", ["server","watcher","minified"]);
